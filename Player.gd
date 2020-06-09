b77f4dd2fc50b38453f2ede7b50e80ed7f58e1ca
willieLjohnson <liwa.johnson@gmail.com>
2020-06-09 10:31:31 -0400

    Implement very basic movement

--- Player.gd
+++ Player.gd
@@ -1,6 +1,20 @@
 extends KinematicBody2D
 
+var velocity = Vector2()
 
 func _physics_process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 30
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -30
+	else:
+		velocity.x = 0
 	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -30
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 30
+	else:
+		velocity.y = 0
+		
+	move_and_slide(velocity)
